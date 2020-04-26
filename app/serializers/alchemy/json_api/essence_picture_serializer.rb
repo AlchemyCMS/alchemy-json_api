ab887f2b4f0f5d7ad13e16660f9fcde804f8908b
Martin Meyerhoff <mamhoff@gmail.com>
2020-04-26 20:27:00 +0200

    Add EssencePicture Serializer

---
+++ app/serializers/alchemy/json_api/essence_picture_serializer.rb
@@ -0,0 +1,51 @@
+require "alchemy/json_api/essence_serializer"
+require "alchemy/json_api/link_helper"
+
+module Alchemy::JsonApi
+  class EssencePictureSerializer
+    include EssenceSerializer
+    extend LinkHelper
+
+    attributes(
+      :title,
+      :caption,
+      :alt_tag,
+      :css_class,
+      :link_title,
+      :link_class_name,
+      :link_target
+    )
+
+    link :url do |essence|
+      essence.picture_url
+    end
+
+    link :href do |essence|
+      essence.link
+    end
+
+    attribute :name do |essence|
+      essence.picture.name
+    end
+
+    attribute :file_name do |essence|
+      essence.picture.image_file_name
+    end
+
+    attribute :mime_type do |essence|
+      "image/#{essence.picture.image_file_format}"
+    end
+
+    attribute :size do |essence|
+      essence.picture.image_file_size
+    end
+
+    attribute :dimensions do |essence|
+      essence.picture.image_size
+    end
+
+    attribute :tag_list do |essence|
+      essence.picture.tag_list
+    end
+  end
+end
---
+++ spec/serializers/alchemy/json_api/essence_picture_serializer_spec.rb
@@ -0,0 +1,45 @@
+require "rails_helper"
+require "alchemy/test_support/factories/content_factory"
+require "alchemy/test_support/factories/essence_picture_factory"
+
+RSpec.describe Alchemy::JsonApi::EssencePictureSerializer do
+  let(:content) { FactoryBot.create(:alchemy_content) }
+  let(:essence) do
+    FactoryBot.create(
+      :alchemy_essence_picture,
+      title: "Picture",
+      css_class: "custom",
+      content: content,
+      link: "/hello",
+    )
+  end
+  let(:options) { {} }
+
+  subject(:serializer) { described_class.new(essence, options) }
+
+  it_behaves_like "an essence"
+
+  describe "attributes" do
+    subject { serializer.serializable_hash[:data][:attributes] }
+
+    it "has the right keys and values" do
+      expect(subject[:title]).to eq("Picture")
+      expect(subject[:css_class]).to eq("custom")
+      expect(subject[:name]).to eq("image")
+      expect(subject[:file_name]).to eq("image.png")
+      expect(subject[:mime_type]).to eq("image/png")
+      expect(subject[:size]).to eq(70)
+      expect(subject[:dimensions]).to eq(height: 1, width: 1)
+      expect(subject[:tag_list]).to eq([])
+    end
+  end
+
+  describe "links" do
+    subject { serializer.serializable_hash[:data][:links] }
+
+    it "has the right keys and values" do
+      expect(subject[:url]).to match(/\/pictures\/\w+\/image\.png\?sha=\w+/)
+      expect(subject[:href]).to eq("/hello")
+    end
+  end
+end
