281d87a10d1337009440765d12eb536c92e20792
Martin Meyerhoff <mamhoff@gmail.com>
2020-05-04 15:37:11 +0200

    Add Essence Node Serializer
    This does serialize the node as a relationship to the essence. We can't
    reasonably make it such that all descendants are side loaded without the
    relationship.

---
+++ app/serializers/alchemy/json_api/essence_node_serializer.rb
@@ -0,0 +1,32 @@
+require "alchemy/json_api/essence_serializer"
+require "alchemy/json_api/link_helper"
+
+module Alchemy::JsonApi
+  class EssenceNodeSerializer
+    include EssenceSerializer
+
+    attribute :ingredient do |essence|
+      essence&.node&.name
+    end
+
+    belongs_to :node
+
+    with_options if: Proc.new { |essence| essence.node.present? } do
+      attribute :name do |essence|
+        essence.node.name
+      end
+
+      attribute :link_url do |essence|
+        essence.node.url
+      end
+
+      attribute :link_title do |essence|
+        essence.node.title
+      end
+
+      attribute :link_nofollow do |essence|
+        essence.node.nofollow
+      end
+    end
+  end
+end
---
+++ spec/serializers/alchemy/json_api/essence_node_serializer_spec.rb
@@ -0,0 +1,51 @@
+require "rails_helper"
+require "alchemy/test_support/factories"
+require "alchemy/essence_node"
+
+RSpec.describe Alchemy::JsonApi::EssenceNodeSerializer do
+  let(:element) { FactoryBot.create(:alchemy_element) }
+  let(:content) { FactoryBot.create(:alchemy_content, element: element) }
+  let(:essence) { Alchemy::EssenceNode.create(node: node, content: content) }
+  let(:node) { FactoryBot.create(:alchemy_node, title: "Pop-up explanation", url: "/acdc", nofollow: true, children: [child_node]) }
+  let(:child_node) { FactoryBot.create(:alchemy_node, children: [child_of_child_node]) }
+  let(:child_of_child_node ) { FactoryBot.create(:alchemy_node) }
+  let(:options) { {} }
+
+  subject(:serializer) { described_class.new(essence, options) }
+
+  it_behaves_like "an essence"
+
+  describe "attributes" do
+    subject { serializer.serializable_hash[:data][:attributes] }
+
+    it "has the right keys and values" do
+      expect(subject[:ingredient]).to eq(node.name)
+      expect(subject[:name]).to eq("A Node")
+      expect(subject[:link_title]).to eq("Pop-up explanation")
+      expect(subject[:link_url]).to eq("/acdc")
+      expect(subject[:link_nofollow]).to be true
+    end
+  end
+
+  describe "relationships" do
+    subject { serializer.serializable_hash[:data][:relationships] }
+
+    it "has the right keys and values" do
+      expect(subject[:node]).to eq(data: { id: node.id.to_s, type: :node })
+    end
+  end
+
+  context "With no node set" do
+    let(:essence) { Alchemy::EssenceNode.create(node: nil, content: content) }
+
+    it_behaves_like "an essence"
+
+    describe "attributes" do
+      subject { serializer.serializable_hash[:data][:attributes] }
+
+      it "has the right keys and values" do
+        expect(subject[:ingredient]).to be nil
+      end
+    end
+  end
+end
