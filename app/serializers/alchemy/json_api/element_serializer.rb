68b4b8d9ee2bb70f83324cafa81c956e0f2c10c7
Martin Meyerhoff <mamhoff@gmail.com>
2020-05-04 13:49:23 +0200

    Add parent, nested elements, and position to element serializer

--- app/serializers/alchemy/json_api/element_serializer.rb
+++ app/serializers/alchemy/json_api/element_serializer.rb
@@ -2,16 +2,20 @@ module Alchemy::JsonApi
   class ElementSerializer
     include FastJsonapi::ObjectSerializer
     attributes(
+      :position,
       :tag_list,
       :created_at,
       :updated_at
     )
     attribute :display_name, &:display_name_with_preview_text
     attribute :element_type, &:name
+    belongs_to :parent_element, record_type: :element
 
     belongs_to :page
     has_many :essences, polymorphic: true do |element|
       element.contents.map(&:essence)
     end
+
+    has_many :nested_elements, record_type: :element, serializer: self
   end
 end
--- spec/serializers/alchemy/json_api/element_serializer_spec.rb
+++ spec/serializers/alchemy/json_api/element_serializer_spec.rb
@@ -7,8 +7,12 @@ RSpec.describe Alchemy::JsonApi::ElementSerializer do
       :alchemy_element,
       autogenerate_contents: true,
       tag_list: "Tag1,Tag2",
+      nested_elements: [nested_element],
+      parent_element: parent_element
     )
   end
+  let(:nested_element) { FactoryBot.create(:alchemy_element) }
+  let(:parent_element) { FactoryBot.create(:alchemy_element) }
   let(:options) { {} }
 
   subject(:serializer) { described_class.new(element, options) }
@@ -20,6 +24,7 @@ RSpec.describe Alchemy::JsonApi::ElementSerializer do
       expect(subject[:element_type]).to eq("article")
       expect(subject[:created_at]).to eq(element.created_at)
       expect(subject[:updated_at]).to eq(element.updated_at)
+      expect(subject[:position]).to eq(element.position)
       expect(subject[:tag_list]).to eq(["Tag1", "Tag2"])
       expect(subject[:display_name]).to eq("Article: ")
     end
@@ -31,6 +36,8 @@ RSpec.describe Alchemy::JsonApi::ElementSerializer do
     it "has the right keys and values" do
       expect(subject[:page]).to eq(data: { id: element.page_id.to_s, type: :page })
       expect(subject[:essences]).to eq(data: element.contents.map { |c| {id: c.essence_id.to_s, type: c.essence.class.name.demodulize.underscore.to_sym} })
+      expect(subject[:nested_elements]).to eq(data: [{id: nested_element.id.to_s, type: :element}])
+      expect(subject[:parent_element]).to eq(data: {id: parent_element.id.to_s, type: :element})
     end
   end
 end
