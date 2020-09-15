deac9a0557572003f11db0586db18583bd55f8dc
Martin Meyerhoff <mamhoff@gmail.com>
2020-09-15 11:48:35 +0200

    Specify serializer for Node parents
    Node parents are nodes, thus they can use the node serializer.

--- app/serializers/alchemy/json_api/node_serializer.rb
+++ app/serializers/alchemy/json_api/node_serializer.rb
@@ -9,7 +9,7 @@ module Alchemy
       attribute :link_title, &:title
       attribute :link_nofollow, &:nofollow
 
-      belongs_to :parent, record_type: :node
+      belongs_to :parent, record_type: :node, serializer: self
 
       has_many :children, record_type: :node, serializer: self
     end
