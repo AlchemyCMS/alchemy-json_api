7ad18e73ce5981e6409b36b7cd7681a6d5881955
Martin Meyerhoff <mamhoff@gmail.com>
2020-11-13 15:53:11 +0100

    Log errors into standard out
    We want better error visibility.

--- app/controllers/alchemy/json_api/base_controller.rb
+++ app/controllers/alchemy/json_api/base_controller.rb
@@ -7,6 +7,23 @@ module Alchemy
       include JSONAPI::Errors
       include JSONAPI::Filtering
       include JSONAPI::Pagination
+
+      private
+
+      def render_jsonapi_internal_server_error(exception)
+        log_error(exception)
+        super
+      end
+
+      def log_error(exception)
+        logger = Rails.logger
+        return unless logger
+
+        message = +"\n#{exception.class} (#{exception.message}):\n"
+        message << exception.annotated_source_code.to_s if exception.respond_to?(:annotated_source_code)
+        message << "  " << exception.backtrace.join("\n  ")
+        logger.fatal("#{message}\n\n")
+      end
     end
   end
 end
