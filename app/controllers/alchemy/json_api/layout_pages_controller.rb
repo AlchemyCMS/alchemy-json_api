ac38deef407ce2559fa1fadbb5efc307ba64f57b
Thomas von Deyen <thomas@vondeyen.com>
2021-03-05 14:14:12 +0100

    Do not return unpublished layoutpages
    Unauthentiated users must not see unpublished layoutpages.

--- app/controllers/alchemy/json_api/layout_pages_controller.rb
+++ app/controllers/alchemy/json_api/layout_pages_controller.rb
@@ -4,10 +4,6 @@ module Alchemy
     class LayoutPagesController < JsonApi::PagesController
       private
 
-      def base_page_scope
-        ::Alchemy::Page.all
-      end
-
       def page_scope
         page_scope_with_includes.layoutpages
       end
