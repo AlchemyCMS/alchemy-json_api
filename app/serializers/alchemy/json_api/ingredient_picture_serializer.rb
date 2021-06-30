2e98dd3e1773a6339ccc33fe89a35f846c8555b5
Thomas von Deyen <thomas@vondeyen.com>
2021-06-30 17:05:11 +0200

    Add Ingredient Picture serializer

---
+++ app/serializers/alchemy/json_api/ingredient_picture_serializer.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+require "alchemy/json_api/ingredient_serializer"
+
+module Alchemy
+  module JsonApi
+    class IngredientPictureSerializer
+      include IngredientSerializer
+
+      attributes(
+        :title,
+        :caption,
+        :link_class_name,
+        :link_title,
+        :link_target,
+      )
+
+      attribute :value do |ingredient|
+        ingredient.picture&.url
+      end
+
+      attribute :alt_text, &:alt_tag
+      attribute :link_url, &:link
+
+      with_options if: proc { |ingredient| ingredient.picture.present? } do
+        attribute :image_dimensions do |ingredient|
+          sizes = ingredient.settings[:size]&.split("x", 2)&.map(&:to_i) || [
+            ingredient.image_file_width,
+            ingredient.image_file_height,
+          ]
+
+          ratio = ingredient.image_file_width.to_f / ingredient.image_file_height
+          width = sizes[0].zero? ? sizes[1] * ratio : sizes[0]
+          height = sizes[1].zero? ? sizes[0] / ratio : sizes[1]
+
+          {
+            width: width,
+            height: height,
+          }
+        end
+
+        attribute :image_name do |ingredient|
+          ingredient.picture.name
+        end
+
+        attribute :image_file_name do |ingredient|
+          ingredient.picture.image_file_name
+        end
+
+        attribute :image_mime_type do |ingredient|
+          "image/#{ingredient.picture.image_file_format}"
+        end
+
+        attribute :image_file_size do |ingredient|
+          ingredient.picture.image_file_size
+        end
+      end
+    end
+  end
+end
---
+++ spec/serializers/alchemy/json_api/ingredient_picture_serializer_spec.rb
@@ -0,0 +1,96 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Alchemy::JsonApi::IngredientPictureSerializer do
+  let(:ingredient) do
+    FactoryBot.build_stubbed(
+      :alchemy_ingredient_picture,
+      title: "Picture",
+      link: "/hello",
+      picture: picture,
+    )
+  end
+
+  let(:picture) { FactoryBot.create(:alchemy_picture, image_file_size: 301) }
+
+  subject(:serializer) { described_class.new(ingredient) }
+
+  it_behaves_like "an ingredient serializer"
+
+  describe "attributes" do
+    subject { serializer.serializable_hash[:data][:attributes] }
+
+    it "has the right keys and values" do
+      expect(subject[:title]).to eq("Picture")
+      expect(subject[:value]).to match(/pictures\/\w+\/image.png/)
+      expect(subject[:image_name]).to eq("image")
+      expect(subject[:image_file_name]).to eq("image.png")
+      expect(subject[:image_mime_type]).to eq("image/png")
+      expect(subject[:image_file_size]).to eq(301)
+      expect(subject[:image_dimensions]).to eq(width: 1, height: 1)
+    end
+
+    describe "image_dimensions" do
+      let(:image_dimensions) { subject[:image_dimensions] }
+
+      context "without image" do
+        let(:picture) { nil }
+
+        it { expect(image_dimensions).to be_nil }
+      end
+
+      context "with image" do
+        it do
+          expect(image_dimensions).to eq(width: 1, height: 1)
+        end
+
+        context "with ingredient settings[:size]" do
+          before do
+            expect(ingredient).to receive(:settings).at_least(:once) { size }
+          end
+
+          let(:size) do
+            { size: "100x100" }
+          end
+
+          it do
+            expect(image_dimensions).to eq(width: 100, height: 100)
+          end
+
+          context "without y dimension" do
+            let(:size) do
+              { size: "100x" }
+            end
+
+            it "infers height from ratio" do
+              expect(image_dimensions).to eq(width: 100, height: 100)
+            end
+          end
+
+          context "without x dimension" do
+            let(:size) do
+              { size: "x50" }
+            end
+
+            it "infers width from ratio" do
+              expect(image_dimensions).to eq(width: 50, height: 50)
+            end
+          end
+        end
+      end
+    end
+  end
+
+  context "With no picture set" do
+    let(:picture) { nil }
+
+    describe "attributes" do
+      subject { serializer.serializable_hash[:data][:attributes] }
+
+      it "has the right keys and values" do
+        expect(subject[:value]).to be_nil
+      end
+    end
+  end
+end
