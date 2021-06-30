302cf849440784b1ad927459d9cc3ca8f7fc58af
Thomas von Deyen <thomas@vondeyen.com>
2021-06-30 17:05:08 +0200

    Add Ingredient Page serializer

---
+++ spec/serializers/alchemy/json_api/ingredient_page_serializer_spec.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Alchemy::JsonApi::IngredientPageSerializer do
+  let(:ingredient) { FactoryBot.build_stubbed(:alchemy_ingredient_page, page: page) }
+  let(:page) { FactoryBot.build_stubbed(:alchemy_page) }
+
+  let(:serializer) { described_class.new(ingredient) }
+
+  it_behaves_like "an ingredient serializer"
+
+  subject { serializer.serializable_hash[:data][:attributes] }
+
+  describe "attributes" do
+    it "has page url as ingredient" do
+      expect(subject[:value]).to eq(page.url_path)
+    end
+
+    it "has page_name" do
+      expect(subject[:page_name]).to eq(page.name)
+    end
+
+    it "has page_url" do
+      expect(subject[:page_url]).to eq(page.url_path)
+    end
+  end
+
+  describe "relationships" do
+    subject { serializer.serializable_hash[:data][:relationships] }
+
+    it "has page object" do
+      expect(subject[:page]).to eq(data: { id: page.id.to_s, type: :page })
+    end
+  end
+
+  context "With no page set" do
+    let(:page) { nil }
+
+    describe "attributes" do
+      it "has no ingredient" do
+        expect(subject[:value]).to be_nil
+      end
+
+      it "has no page_name" do
+        expect(subject[:page_name]).to be_nil
+      end
+
+      it "has no page_url" do
+        expect(subject[:page_url]).to be_nil
+      end
+    end
+  end
+end
