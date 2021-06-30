e71635ecc53df8233d1cae122a46f5646f4fcbae
Thomas von Deyen <thomas@vondeyen.com>
2021-06-30 17:05:19 +0200

    Add Ingredient Select serializer

---
+++ app/serializers/alchemy/json_api/ingredient_select_serializer.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "alchemy/json_api/ingredient_serializer"
+
+module Alchemy
+  module JsonApi
+    class IngredientSelectSerializer
+      include IngredientSerializer
+    end
+  end
+end
