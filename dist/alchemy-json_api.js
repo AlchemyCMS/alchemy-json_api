b83b39e57a7c14531ae55834689c5e75282138a2
Thomas von Deyen <thomas@vondeyen.com>
2022-09-07 08:43:53 +0200

    Add dist files
    We want to be able to use the git url in apps

---
+++ dist/alchemy-json_api.js
@@ -0,0 +1,123 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var structuredClone = require('@ungap/structured-clone');
+
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var structuredClone__default = /*#__PURE__*/_interopDefaultLegacy(structuredClone);
+
+function deserialize(originalResponse, options = {}) {
+  const response = structuredClone__default["default"](originalResponse);
+
+  if (!options) {
+    options = {};
+  }
+
+  const included = response.included || [];
+
+  if (Array.isArray(response.data)) {
+    return response.data.map(data => {
+      return parseJsonApiSimpleResourceData(data, included, false);
+    });
+  } else {
+    return parseJsonApiSimpleResourceData(response.data, included, false);
+  }
+}
+
+function parseJsonApiSimpleResourceData(data, included, useCache, options) {
+  if (!included.cached) {
+    included.cached = {};
+  }
+
+  if (!(data.type in included.cached)) {
+    included.cached[data.type] = {};
+  }
+
+  if (useCache && data.id in included.cached[data.type]) {
+    return included.cached[data.type][data.id];
+  }
+
+  const attributes = data.attributes || {};
+  const resource = attributes;
+  resource.id = data.id;
+  included.cached[data.type][data.id] = resource;
+
+  if (data.relationships) {
+    for (const relationName of Object.keys(data.relationships)) {
+      const relationRef = data.relationships[relationName];
+
+      if (Array.isArray(relationRef.data)) {
+        const items = [];
+        relationRef.data.forEach(relationData => {
+          const item = findJsonApiIncluded(included, relationData.type, relationData.id);
+          items.push(item);
+        });
+        resource[relationName] = items;
+      } else if (relationRef && relationRef.data) {
+        resource[relationName] = findJsonApiIncluded(included, relationRef.data.type, relationRef.data.id);
+      } else {
+        resource[relationName] = null;
+      }
+    }
+  }
+
+  return resource;
+}
+
+function findJsonApiIncluded(included, type, id, options) {
+  let found = null;
+  included.forEach(item => {
+    if (item.type === type && item.id === id) {
+      found = parseJsonApiSimpleResourceData(item, included, true);
+    }
+  });
+
+  if (!found) {
+    found = {
+      id
+    };
+  }
+
+  return found;
+}
+
+function filterDeprecatedElements(elements) {
+  const els = [];
+  elements.forEach(element => {
+    if (element.nested_elements.length > 0) {
+      element.nested_elements = filterDeprecatedElements(element.nested_elements);
+    }
+
+    if (element.essences.length > 0) {
+      element.essences = element.essences.filter(essence => {
+        return !essence.deprecated;
+      });
+    }
+
+    if (!element.deprecated) {
+      els.push(element);
+    }
+  });
+  return els;
+} // Returns deserialized page without deprecated content
+
+
+function deserializePage(pageData) {
+  const page = deserialize(pageData);
+  page.elements = filterDeprecatedElements(page.elements);
+  return page;
+} // Returns deserialized pages without deprecated content
+
+function deserializePages(pagesData) {
+  const pages = deserialize(pagesData);
+  pages.forEach(page => {
+    page.elements = filterDeprecatedElements(page.elements);
+  });
+  return pages;
+}
+
+exports.deserialize = deserialize;
+exports.deserializePage = deserializePage;
+exports.deserializePages = deserializePages;
