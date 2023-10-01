7c10066e29cdbf0f31b3b03db9d4b5bf81fe1dda
Niklas Bichinger <niklas@bichinger.de>
2023-10-01 11:25:08 +0200

    another overloading attempt

--- lib/alchemy/json_api.rb
+++ lib/alchemy/json_api.rb
@@ -5,6 +5,22 @@ require "alchemy_cms"
 require "alchemy/json_api/engine"
 require "alchemy/json_api/page_class_methods_extension"
 
+require File.expand_path('../../app/models/alchemy/page', Alchemy::Engine.called_from)
+
+module Alchemy
+  class Page < BaseRecord
+    class << self
+      def ransackable_attributes(_auth_object = nil)
+        super | %w[page_layout]
+      end
+
+      def ransackable_associations(_auth_object = nil)
+        super | %w[public_version]
+      end
+    end
+  end
+end
+
 module Alchemy
   module JsonApi
     # Set FastJsonapi key_transform
@@ -24,12 +40,6 @@ module Alchemy
     # Default :underscore # "some_key" => "some_key"
     def self.key_transform
       @_key_transform || :underscore
-    end
-
-    class Railtie < ::Rails::Railtie
-      initializer "alchemy-json_api.extend_page_model" do
-        ::Alchemy::Page.extend(Alchemy::JsonApi::PageClassMethodsExtension)
-      end
     end
   end
 end
