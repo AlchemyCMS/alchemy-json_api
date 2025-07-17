30704701aa1a5a21f9e139ae141e9335eb9bc209
Thomas von Deyen <thomas@vondeyen.com>
2025-07-17 22:51:31 +0200

    Update dist
    Vite has been updated to v7 and the dist is now different

--- dist/alchemy-json_api.js
+++ dist/alchemy-json_api.js
@@ -1,25 +1,24 @@
-import { deserialize as d } from "./deserialize.js";
+import { deserialize as r } from "./deserialize.js";
 function n(t) {
   const s = [];
   return t.forEach((e) => {
-    var r, i, c;
-    ((r = e.nested_elements) == null ? void 0 : r.length) > 0 && (e.nested_elements = n(
+    e.nested_elements?.length > 0 && (e.nested_elements = n(
       e.nested_elements
-    )), ((i = e.nestedElements) == null ? void 0 : i.length) > 0 && (e.nestedElements = n(e.nestedElements)), ((c = e.essences) == null ? void 0 : c.length) > 0 && (e.essences = e.essences.filter((f) => !f.deprecated)), e.deprecated || s.push(e);
+    )), e.nestedElements?.length > 0 && (e.nestedElements = n(e.nestedElements)), e.essences?.length > 0 && (e.essences = e.essences.filter((i) => !i.deprecated)), e.deprecated || s.push(e);
   }), s;
 }
-function o(t) {
-  const s = d(t);
+function d(t) {
+  const s = r(t);
   return s.elements = n(s.elements), s;
 }
-function l(t) {
-  const s = d(t);
+function f(t) {
+  const s = r(t);
   return s.forEach((e) => {
     e.elements = n(e.elements);
   }), s;
 }
 export {
-  d as deserialize,
-  o as deserializePage,
-  l as deserializePages
+  r as deserialize,
+  d as deserializePage,
+  f as deserializePages
 };
