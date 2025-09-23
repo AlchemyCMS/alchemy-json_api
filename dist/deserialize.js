00b837ac206a8283f5cf2382f2ba4c4ecbb1c51f
Thomas von Deyen <thomas@vondeyen.com>
2025-09-23 11:20:48 +0200

    Update bundle after vite update

--- dist/deserialize.js
+++ dist/deserialize.js
@@ -1,45 +1,44 @@
-import y from "@ungap/structured-clone";
-function A(e, r = {}) {
-  const s = y(e);
-  r || (r = {});
-  const n = s.included || [];
-  return Array.isArray(s.data) ? s.data.map((i) => f(i, n, !1)) : f(
+function y(e, t = {}) {
+  const s = structuredClone(e);
+  t || (t = {});
+  const o = s.included || [];
+  return Array.isArray(s.data) ? s.data.map((i) => f(i, o, !1)) : f(
     s.data,
-    n,
+    o,
     !1
   );
 }
-function f(e, r, s, n) {
-  if (r.cached || (r.cached = {}), e.type in r.cached || (r.cached[e.type] = {}), s && e.id in r.cached[e.type])
-    return r.cached[e.type][e.id];
-  const t = e.attributes || {};
-  if (t.id = e.id, r.cached[e.type][e.id] = t, e.relationships)
+function f(e, t, s, o) {
+  if (t.cached || (t.cached = {}), e.type in t.cached || (t.cached[e.type] = {}), s && e.id in t.cached[e.type])
+    return t.cached[e.type][e.id];
+  const r = e.attributes || {};
+  if (r.id = e.id, t.cached[e.type][e.id] = r, e.relationships)
     for (const c of Object.keys(e.relationships)) {
-      const o = e.relationships[c];
-      if (Array.isArray(o.data)) {
+      const n = e.relationships[c];
+      if (Array.isArray(n.data)) {
         const p = [];
-        o.data.forEach((a) => {
+        n.data.forEach((a) => {
           const h = u(
-            r,
+            t,
             a.type,
             a.id
           );
           p.push(h);
-        }), t[c] = p;
-      } else o && o.data ? t[c] = u(
-        r,
-        o.data.type,
-        o.data.id
-      ) : t[c] = null;
+        }), r[c] = p;
+      } else n && n.data ? r[c] = u(
+        t,
+        n.data.type,
+        n.data.id
+      ) : r[c] = null;
     }
-  return t;
+  return r;
 }
-function u(e, r, s, n) {
+function u(e, t, s, o) {
   let i = null;
-  return e.forEach((t) => {
-    t.type === r && t.id === s && (i = f(t, e, !0));
+  return e.forEach((r) => {
+    r.type === t && r.id === s && (i = f(r, e, !0));
   }), i || (i = { id: s }), i;
 }
 export {
-  A as deserialize
+  y as deserialize
 };
